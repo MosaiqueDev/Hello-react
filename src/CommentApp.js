9f3ad513a3b34e345480489045da201bcae12830
MosaiqueDev <MosaiqueDev@163.com>
2018-04-17 12:43:15 +0800

    第二阶段

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -3,26 +3,49 @@ import CommentInput from './CommentInput';
 import CommentList from './CommentList';
 
 class CommentApp extends Component {
-    constructor () {
+    constructor() {
         super()
         this.state = {
-            comments:[]
+            comments: []
         }
     }
-    handleSubmitComment (comment) {
+    componentWillMount() {
+        this._loadComments()
+    }
+    _saveComments(comments) {
+        localStorage.setItem('comments', JSON.stringify(comments))
+    }
+    _loadComments() {
+        let comments = localStorage.getItem('comments')
+        if (comments) {
+            comments = JSON.parse(comments)
+            this.setState({ comments })
+        }
+    }
+    handleSubmitComment(comment) {
         if (!comment) return
         if (!comment.username) return alert('请输入用户名哦')
         if (!comment.content) return alert('请输入评论内容')
-        this.state.comments.push(comment);
-        this.setState({
-            comments:this.state.comments
-        })
+        const comments = this.state.comments
+        comments.push(comment);
+        this.setState({ comments })
+        this._saveComments(comments)
     }
-    render () {
+    handleDeleteComment(index) {
+        // console.log(index)
+        const comments = this.state.comments
+        comments.splice(index, 1)
+        this.setState({ comments })
+        this._saveComments(comments)
+    }
+    render() {
         return (
             <div className='wrapper'>
-                <CommentInput onSubmit={this.handleSubmitComment.bind(this)}/>
-                <CommentList comments={this.state.comments}/>
+                <CommentInput onSubmit={this.handleSubmitComment.bind(this)} />
+                <CommentList
+                    comments={this.state.comments}
+                    onDeleteComment={this.handleDeleteComment.bind(this)}
+                />
             </div>
         );
     }
